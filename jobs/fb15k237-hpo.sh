80c5592a9723ab70c6fb93e3cb6a4537752bce57
Daniel <dfdazac@gmail.com>
2024-05-19 13:29:58 +0200

    Add FB15k237 job configurations

---
+++ jobs/fb15k237-hpo.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --job-name=mpqepp
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=18
+#SBATCH --time=02:00:00
+#SBATCH --mem=60G
+#SBATCH --partition=gpu
+#SBATCH --gpus=1
+#SBATCH --output=log_test_%A_%a.out
+
+source activate hqeqs
+hqe train --dataset fb15k_237_betae -tr "/*/*:*" -va "/*/*:*" -te "/*/*:*" -e 3 -b 64 -w
