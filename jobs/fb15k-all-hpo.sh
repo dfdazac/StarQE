30c4c1fcf5d09f2bcc122b771c3e1112d5f0dfaf
Daniel <dfdazac@gmail.com>
2024-05-20 22:10:02 +0200

    Add sweeps for FB15k and NELL995 on all data

---
+++ jobs/fb15k-all-hpo.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --job-name=fb15k-sweep
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=18
+#SBATCH --time=15:00:00
+#SBATCH --mem=60G
+#SBATCH --partition=gpu
+#SBATCH --gpus=1
+#SBATCH --output=log_test_%A_%a.out
+
+PROJ_FOLDER=mpqepp
+
+# Copy data to scratch
+cp -r $HOME/$PROJ_FOLDER $TMPDIR
+cd $TMPDIR/$PROJ_FOLDER
+
+source activate hqeqs
+wandb agent --count 1 dfdazac/mpqepp/j8sgitz9
